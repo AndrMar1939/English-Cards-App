c188a93867ea395b93dffc966772d34f63f87467
AndrMar1939 <advo737@gmail.com>
2023-05-09 16:27:40 +0300

    change url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const API_KEY = import.meta.env.VITE_KEY;
-const URL = import.meta.env.VITE_URL;
-const api = createClient(URL, API_KEY)
+const URL_SUPABASE = import.meta.env.VITE_URL;
+const api = createClient(URL_SUPABASE, API_KEY)
 
 async function getApi(category) {
     let { data, error } = await api
