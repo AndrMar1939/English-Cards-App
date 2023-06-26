ff57fc4225a9c0c0321252d1cfbb54e6ebef276b
AndrMar1939 <advo737@gmail.com>
2023-06-26 18:10:42 +0300

    commit for CV

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 
 const API_KEY = import.meta.env.VITE_KEY;
 const URL_SUPABASE = import.meta.env.VITE_URL;
+
 const api = createClient(URL_SUPABASE, API_KEY);
+
 
 async function apiSupaBase(category) {
     let { data, error } = await api
